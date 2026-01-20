413010360765976c7c72e34c6ac8fe54b2b5d2ec
Sajan Sharma <sajansharmanz@gmail.com>
2026-01-20 23:33:13 +1300

    Fix vscode.sh script

--- themes/monochrome/vscode.sh
+++ themes/monochrome/vscode.sh
@@ -2,4 +2,4 @@
 
 export VSC_THEME="Pitch Black"
 export VSC_EXTENSION="viktorqvarfordt.vscode-pitch-black-theme"
-source $SAJANS_PATH/themes/set-vscode-theme.sh
+source $SAJANS_UBUNTU_PATH/themes/set-vscode-theme.sh
