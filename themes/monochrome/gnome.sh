a2c6f5ed869266faf286aa76549c001c6aaa833b
Sajan Sharma <sajansharmanz@gmail.com>
2026-01-20 23:32:41 +1300

    Fix gnome.sh script

--- themes/monochrome/gnome.sh
+++ themes/monochrome/gnome.sh
@@ -2,4 +2,4 @@
 
 SAJANS_THEME_COLOR="black"
 SAJANS_THEME_BACKGROUND="monochrome/background.jpg"
-source $SAJANS_PATH/themes/set-gnome-theme.sh
+source $SAJANS_UBUNTU_PATH/themes/set-gnome-theme.sh
